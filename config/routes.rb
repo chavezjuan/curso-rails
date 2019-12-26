d20b887be04ea0081734c2736858c926da613528
Juan Chavez Dev <jctecenge@gmail.com>
2019-12-26 15:14:58 -0200

    criado um novo template, action e route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'pages/home', to:'pages#home'
   get 'pages/about', to:'pages#about'
   root'pages#home'
+
+  resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
