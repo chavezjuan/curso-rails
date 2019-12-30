6be3ecbf09661c0bdc79db6637b91cb4c9cbacf0
Juan Chavez Dev <jctecenge@gmail.com>
2019-12-30 15:32:55 -0200

    Corrigido o create, adicionado o flash e criado mensagens de erro, criado show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,23 @@ class ArticlesController < ApplicationController
   end
   
   def create
+    # Outro método para realizar a destinação do artigo
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    #@article.save
+    #redirect_to article_path(@article)
+    #redirect_to articles_show(@article)
+
+    if @article.save
+      flash[:notice] = "O artigo foi criado com sucesso"
+      redirect_to article_path(@article)
+    else 
+      render 'new'
+    end
+  end
+
+  def show
+    @article = Article.find(params[:id])
   end
 
   private
